4c31d4bc4ab49b9199ab3aff48b23179951b6c45
B-Naoufal <naoufalbrahmi@egmail.com>
2025-01-26 01:08:10 +0100

    Refactor: sql and storer code base

--- finowl-backend/pkg/storer/tweet_to_ticker.go
+++ finowl-backend/pkg/storer/tweet_to_ticker.go
@@ -22,59 +22,75 @@ func ConvertTweetsToTickers(
 	influencers influencer.InfluencerRankings,
 ) []ticker.Ticker {
 	var tickers []ticker.Ticker
-	for _, tweet := range tweets {
-		tier := DefaultInfluencerTier
-		influencer, _ := influencers.FindInfluencer(tweet.Author)
-		if influencer != nil {
-			tier = influencer.Tier
-		}
-		mentionDetails := ticker.MentionDetails{
-			Influencers: map[string]ticker.MentionDetail{
-				tweet.Author: {
-					Tier:      tier,                      // Default tier value
-					TweetLink: getFirstLink(tweet.Links), // Get the first link from the tweet's links
-					Content:   tweet.Content,
-				},
-			},
-		}
-		timeStamp := parseTimestamp(tweet.Timestamp) // Parse the timestamp
 
+	for _, tweet := range tweets {
+		tier := getInfluencerTier(tweet.Author, influencers)
+		mentionDetails := createMentionDetails(tweet.Author, tier, tweet)
+
+		timeStamp := parseTimestamp(tweet.Timestamp) // Parse the timestamp
 		mindShare, err := mindshare.CalculateMindshare(mentionDetails, ticker.MentionDetails{})
 		if err != nil {
 			fmt.Printf("failed to calculate mindShare: %v", err)
 			return nil
 		}
 
-		for _, tickerSymbol := range tweet.Tickers {
-			// Convert ticker symbol to uppercase
-			tickerSymbol = strings.ToUpper(tickerSymbol)
-
-			// Check if the ticker is excluded
-			if isTickerExcluded(tickerSymbol[1:]) {
-				continue // Skip this ticker if it is in the excluded list
-			}
-
-			// Create a new Ticker for each ticker symbol
-			ticker := ticker.Ticker{
-				TickerSymbol:     tickerSymbol[1:],
-				Category:         mindShare.Category,
-				MindshareScore:   mindShare.Score,
-				LastMentionedAt:  timeStamp, // Parse the timestamp
-				FirstMentionedAt: timeStamp, // Corrected field name
-				MentionDetails: ticker.MentionDetails{
-					Influencers: map[string]ticker.MentionDetail{
-						tweet.Author: {
-							Tier:      tier,                      // Default tier value
-							TweetLink: getFirstLink(tweet.Links), // Get the first link from the tweet's links
-							Content:   tweet.Content,
-						},
-					},
-				},
-			}
-			tickers = append(tickers, ticker)
-		}
+		tickers = appendTickers(tickers, tweet.Tickers, *mindShare, timeStamp, tier, tweet)
 	}
 
+	return tickers
+}
+
+// getInfluencerTier retrieves the influencer tier for a given author
+func getInfluencerTier(author string, influencers influencer.InfluencerRankings) int {
+	influencer, _ := influencers.FindInfluencer(author)
+	if influencer != nil {
+		return influencer.Tier
+	}
+	return DefaultInfluencerTier
+}
+
+// createMentionDetails creates mention details for a given author and tier
+func createMentionDetails(author string, tier int, tweet Tweet) ticker.MentionDetails {
+
+	// Create the MentionDetails
+	mentionDetails := ticker.MentionDetails{
+		Influencers: map[string]ticker.MentionDetail{
+			author: {
+				Tier:      tier,                      // Default tier value
+				TweetLink: getFirstLink(tweet.Links), // Get the first link from the tweet's links
+				Content:   tweet.Content,
+			},
+		},
+	}
+
+	return mentionDetails // Return both mention details and tier counts
+}
+
+// appendTickers appends new tickers to the existing slice based on the tweet's tickers
+func appendTickers(
+	tickers []ticker.Ticker, tickerSymbols []string, mindShare mindshare.Mindshare, timeStamp time.Time, tier int, tweet Tweet) []ticker.Ticker {
+	for _, tickerSymbol := range tickerSymbols {
+		// Convert ticker symbol to uppercase
+		tickerSymbol = strings.ToUpper(tickerSymbol)
+
+		// Check if the ticker is excluded
+		if isTickerExcluded(tickerSymbol[1:]) {
+			continue // Skip this ticker if it is in the excluded list
+		}
+
+		mentionsDetail := createMentionDetails(tweet.Author, tier, tweet)
+
+		// Create a new Ticker for each ticker symbol
+		ticker := ticker.Ticker{
+			TickerSymbol:     tickerSymbol[1:],
+			Category:         mindShare.Category,
+			MindshareScore:   mindShare.Score,
+			LastMentionedAt:  timeStamp, // Parse the timestamp
+			FirstMentionedAt: timeStamp, // Corrected field name
+			MentionDetails:   mentionsDetail,
+		}
+		tickers = append(tickers, ticker)
+	}
 	return tickers
 }
 
