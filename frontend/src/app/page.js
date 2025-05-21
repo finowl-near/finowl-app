6db00073c627672234498008293e5540a9163210
KD-ayoub <ayoubkadi285@gmail.com>
2025-05-21 20:44:07 +0100

    fix: change entry point

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,37 +1,14 @@
-"use client";
-
-import Image from "next/image";
-import { Suspense, useEffect, useState } from "react";
-import { NearContext } from "./context";
-import { Wallet } from "./Wallets/near";
-import { CounterContract, NetworkId } from "./config";
+import Header from "./components/Header";
 import LandingPage from "./components/LandingPage";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
 
-const wallet = new Wallet({
-  createAccessKeyFor: CounterContract,
-  networkId: NetworkId,
-});
 
 export default function Home() {
-  const [signedAccountId, setSignedAccountId] = useState("");
-
-  useEffect(() => {
-    console.log("set up Account");
-    wallet.startUp(setSignedAccountId);
-  }, []);
-
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <NearContext.Provider value={{ signedAccountId, wallet }}>
-          <Suspense>
-            <LandingPage />
-          </Suspense>
-        </NearContext.Provider>
-      </QueryClientProvider>
+        <Header />
+        <LandingPage />
+        {/* <Landing/> */}
     </>
   );
 }
