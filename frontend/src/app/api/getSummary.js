a8963536b52f6d24cfe8bd01becb8e41f8109b52
KD-ayoub <ayoubkadi285@gmail.com>
2025-05-21 22:51:57 +0100

    fix: fetching data files

--- frontend/src/app/api/getSummary.js
+++ frontend/src/app/api/getSummary.js
@@ -1,10 +1,20 @@
 import axios from "axios";
 
 export default async function getSummary(id) {
-    console.log('inside getSummary', id)
-    const response = await axios.get(`https://finowl.finance/api/v0/summary?id=${id}`);
-    if (response.status === 200) {
-        return response.data;
-    }
-    return null;
+    console.log("inside getSummary", id);
+  let response;
+  if (id) {
+    response = await axios.get(
+      `https://finowl.finance/api/v0/summary?id=${id}`
+    );
+} else {
+      response = await axios.get(
+        `https://finowl.finance/api/v0/summary`
+      );
+  }
+  if (response.status === 200) {
+    console.log("data inside summary", response.data);
+    return response.data;
+  }
+  return null;
 }
--- frontend/src/app/api/getTableData.js
+++ frontend/src/app/api/getTableData.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function getTableData(page) {
     console.log("inside page", page)
-    const response = await axios.get(`http://localhost:8080/api/v0/tickers?page=${page}&pageSize=10&sort=last_mentioned&sortDir=desc`);
+    const response = await axios.get(`https://finowl.finance/api/v0/tickers?page=${page}&pageSize=10&sort=last_mentioned&sortDir=desc`);
     if (response.status === 200) {
         return response.data;
     }
