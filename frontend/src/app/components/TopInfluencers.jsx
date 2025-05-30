8e83bd133368814a7d2e9342e647c919c449b591
KD-ayoub <ayoubkadi285@gmail.com>
2025-05-31 00:15:13 +0100

    fix: components fixed

--- frontend/src/app/components/TopInfluencers.jsx
+++ frontend/src/app/components/TopInfluencers.jsx
@@ -1,29 +1,72 @@
 import React, { useEffect, useState } from "react";
 import useTableData from "../hooks/useTableData";
+import { parseInfluencers } from "./Table";
 
-export default function TopInfluencers({ tickerSymbol, influencers }) {
-//   const topInfluencers = useTableData((state) => state.topInfluencers);
-if (!influencers) return null
-  const tickerMap = influencers.get(tickerSymbol);
+function countInfluencersByTier(influencers) {
+  const tierCount = {
+    influencer_tier_1: 0,
+    influencer_tier_2: 0,
+    influencer_tier_3: 0,
+  };
+
+  if (
+    
+    !influencers ||
+    typeof influencers !== "object"
+  ) {
+    return tierCount;
+  }
+
+  for (const influencer in influencers) {
+    const info = influencers[influencer];
+    const tier = info.tier;
+    console.log('influenser tier => ', tier);
+    if (typeof tier === "number" && tier >= 1 && tier <= 3) {
+      tierCount[`influencer_tier_${tier}`]++;
+    }
+  }
+
+  return tierCount;
+}
+
+export default function TopInfluencers({
+  tickerSymbol,
+  influencersData,
+}) {
+  //   const topInfluencers = useTableData((state) => state.topInfluencers);
+  if (!influencersData) return null;
+  const parsedInfluensers = parseInfluencers(influencersData)
+  // const tierCount = countInfluencersByTier(influencers);
+  const tickerMap = parsedInfluensers.get(tickerSymbol);
   const tickerArray = [...tickerMap];
-  console.log("debug", influencers, tickerSymbol, tickerArray);
+  console.log("debug", tickerSymbol, tickerArray);
 
   return (
     <div className="flex items-center justify-center">
       {/* {tickerArray.map((val, idx) => {
         return ( */}
-          <>
-            <div className="w-6 h-6 flex  left-0 z-20 justify-center items-center rounded-full bg-[#FF6347]/30 border border-[#FF6B34]">
-              <p className="text-[#FF6B34] font-semibold text-sm ">{tickerArray[0][1]}</p>
-            </div>
-            <div className="w-6 h-6 mask-sm flex  left-5 z-10 justify-center items-center rounded-full bg-[#1E90FF]/30 border border-[#00BFFF]">
-              <p className="text-[#00BFFF] font-semibold text-sm ">{tickerArray[1][1]}</p>
-            </div>
-            <div className="w-6 h-6 mask-sm flex  left-10 justify-center items-center rounded-full bg-[#32CD32]/30 border border-[#60FF60]">
-              <p className="text-[#60FF60] font-semibold text-sm ">{tickerArray[2][1]}</p>
-            </div>
-          </>
-        {/* );
+      <>
+        <div className="w-6 h-6 flex  left-0 z-20 justify-center items-center rounded-full bg-[#FF6347]/30 border border-[#FF6B34]">
+          <p className="text-[#FF6B34] font-semibold text-sm ">
+            {tickerArray[0][1]}
+            {/* {tierCount.influencer_tier_1} */}
+          </p>
+        </div>
+        <div className="w-6 h-6 mask-sm flex  left-5 z-10 justify-center items-center rounded-full bg-[#1E90FF]/30 border border-[#00BFFF]">
+          <p className="text-[#00BFFF] font-semibold text-sm ">
+            {tickerArray[1][1]}
+            {/* {tierCount.influencer_tier_2} */}
+
+          </p>
+        </div>
+        <div className="w-6 h-6 mask-sm flex  left-10 justify-center items-center rounded-full bg-[#32CD32]/30 border border-[#60FF60]">
+          <p className="text-[#60FF60] font-semibold text-sm ">
+            {tickerArray[2][1]}
+            {/* {tierCount.influencer_tier_3} */}
+          </p>
+        </div>
+      </>
+      {/* );
       })} */}
     </div>
   );
